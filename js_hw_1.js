068be69b548b432a4412f951c0cbf01a76774b49
igbariaothman <igbariaothman@gmail.com>
2025-04-12 19:11:15 +0300

    Add files via upload

---
+++ js_hw_1.js
@@ -0,0 +1,159 @@
+// true or false
+
+//task 1 == yes 
+// if ("0"){
+//   alert('hello') ;
+// }
+
+
+//task 2 
+
+// let x  = prompt("enter a name ");
+
+// if (x == "ECMAScript")
+//   alert("correct");
+// else 
+//   alert("the correct is : ECMAScript ") ;
+
+
+//task 3 
+// let score = 75 ;
+
+// let grade =  score >= 90 ? "A" :
+//                    score >= 80 ? "B" :
+//                    score >= 70 ? "C" : 
+//                    score >= 60 ? "D" : "F" ;
+
+// console.log(`the grade is ${grade}`)
+
+//task4 
+// let num  = prompt("enter number ");
+
+// if (num >= "1"  )
+//   alert ( "1")
+
+// else if (num == "0")
+//   alert ("0")
+
+// else if (num <= "-1-1")
+//   alert ("-1")
+
+//task 5
+// let result ;
+
+// result = a+b <4 ? "not enough":
+//             "A lot" ;
+
+//  alert (result);           
+
+
+//task 6 
+
+// let message ;
+
+// message = login == "employee" ? 'hello' :
+//                   login == 'director' ? 'hello' :
+//                   login == ' ' ? 'no login' :
+//                   ' ' ; 
+
+// task 7 
+
+// 'sdf '
+// false 
+//true
+// ' '
+//null 
+//null
+// undefined
+// 1
+// 1
+// 1
+
+
+// loops 
+//task 1 
+
+// let sum = 0 ;
+//  let i = 1 ; 
+//  while (i < 101){
+//   sum += i ;
+//   i ++ ;
+//  }
+
+
+// task 2
+// let number;
+
+// do {
+//   number = prompt("enter number");
+
+//   // ממיר למספר
+//   number = Number(number);
+
+// } while (isNaN(number) || number <= 0);
+
+// console.log("enter legal number ", number);
+
+//task 3
+
+// let num = 7 ;
+// let i = 1 ;
+
+// for (i ; i < 11 ; i++){
+//   let sum = num * i ;
+//   console.log(`${num} * ${i} = ${sum }`)
+//   sum = 0 ;
+// }
+
+//task 4
+// let x = prompt('enter number between 1 - 10');
+// let num = 7 ;
+// for (let i = 0 ; i < 10 ; i++){
+//   if (x == num ){
+//    alert ('true ') ;
+//    break ;}
+//   else 
+//   x = prompt('not correct ,enter number between 1 - 10');
+
+// }
+
+// task 5 
+// for (let i = 0; i < 31; i++) {
+
+//   if (i % 3 == 0) continue;
+//   console.log(i); 
+// }
+
+//task 6 ==   הוא נכון ואם הוא 0 אז לא נכון אז הלןלאה עוצרת i המספר האחרון הוא 1 כי הללולאה ממשיכה כל עוד ה   
+// let i = 3;
+
+// while (i) {
+//   alert(i--);
+// }
+
+//task 7 == no 
+
+// let i = 0 ;
+// while (++i < 5 ) console.log(i) ;
+// while (i++ < 5) console.log(i);
+
+// task 8 == yes 
+// for (let i = 0 ; i < 5 ; i++) console.log(i);
+// for (let i = 0; i < 5; ++i) console.log(i);
+
+// task 9 
+// for (let i = 2 ; i < 11 ; i++){
+//   if (i % 2 == 0 )
+//     console.log (i)
+// } 
+
+// task 10 
+
+// let i = 0 ;
+// while (i < 3){
+//    alert(`number ${i}!`);
+//     i++;
+// }
+
+
+
